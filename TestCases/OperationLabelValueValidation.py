e88de226351c6192ba8f42ea46beff9afad66259
UditOpsramp <udit.jain@opsramp.com>
2023-02-06 19:50:24 +0530

    Updated

---
+++ TestCases/OperationLabelValueValidation.py
@@ -0,0 +1,53 @@
+#!/usr/lib/python3
+
+import requests
+import yaml
+import time
+import subprocess as sp
+
+
+def OperationLabelValue(workdirectory, AuthToken, tenantid, portal, tracingoperation, starttimemilisec, endtimemilisec, parsedreportfile):
+
+    cmd= "sudo systemctl restart opsramp-agent"
+    sp.getoutput(cmd)
+    
+    time.sleep(30)
+
+    petclinicserverurl = "http://172.25.220.220:8080"
+
+    petclinicresponse = requests.request(
+        "GET", petclinicserverurl)
+
+    time.sleep(60)
+
+    operationurl = "https://"\
+        + portal +\
+        "/tracing-query/api/v1/tenants/"\
+        + tenantid +\
+        '/operations?start='\
+        + str(starttimemilisec) +\
+        "&end="\
+        + str(endtimemilisec)
+
+    payload = {}
+    headers = {
+        'Authorization': AuthToken,
+        'Content-Type': 'application/json'
+    }
+
+    operation_response = requests.request(
+        "GET", operationurl, headers=headers, data=payload)
+
+    if operation_response.status_code == 200:
+        operation_responsejson = operation_response.json()
+        operationslist = operation_responsejson['operations']
+
+        if not operationslist:
+            status = "Validation Fail : Value is not Coming for Tracing Operation Label Attribute"
+            parsedreportfile['OpearationLabelValueStatus'] = status
+        else:
+            status = "Validation Pass : Value is Coming for Tracing Operation Label Attribute"
+            parsedreportfile['OpearationLabelValueStatus'] = status
+
+    with open(workdirectory + "/Report.yml", "w") as file:
+        yaml.dump(parsedreportfile, file)
---
+++ TestCases/ServiceValidation.py
@@ -0,0 +1,51 @@
+#!/usr/lib/python3
+
+import requests
+import yaml
+import time
+import subprocess as sp
+
+def TracingService(workdirectory, AuthToken, tenantid, portal, tracingservice, starttimemilisec, endtimemilisec, parsedreportfile):
+
+    cmd= "sudo systemctl restart opsramp-agent"
+    sp.getoutput(cmd)
+    
+    time.sleep(30)
+
+    petclinicserverurl = "http://172.25.220.220:8080"
+
+    petclinicresponse = requests.request(
+        "GET", petclinicserverurl)
+
+    time.sleep(60)
+
+    traceserviceurl = "https://"\
+        + portal +\
+        "/tracing-query/api/v1/tenants/"\
+        + tenantid +\
+        '/services?start='\
+        + str(starttimemilisec) +\
+        "&end="\
+        + str(endtimemilisec)
+
+    payload = {}
+    headers = {
+        'Authorization': AuthToken,
+        'Content-Type': 'application/json'
+    }
+
+    traceservice_response = requests.request(
+        "GET", traceserviceurl, headers=headers, data=payload)
+    if traceservice_response.status_code == 200:
+        traceservice_responsejson = traceservice_response.json()
+        traceservicenamelist = traceservice_responsejson['services']
+
+        if tracingservice in traceservicenamelist:
+            status = "Validation Pass : Tracing Service " + tracingservice + " is Coming"
+            parsedreportfile['TracingServiceNameStatus'] = status
+        else:
+            status = "Validation Fail : Tracing Service " + tracingservice + " is not Coming"
+            parsedreportfile['TracingServiceNameStatus'] = status
+
+    with open(workdirectory + "/Report.yml", "w") as file:
+        yaml.dump(parsedreportfile, file)
