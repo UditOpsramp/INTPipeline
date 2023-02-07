c5e2ebd5dfa9eace3aa3ea3aa25b0cf42561a58b
UditOpsramp <udit.jain@opsramp.com>
2023-02-07 14:19:02 +0530

    Update

--- ObservabilityAutomation-INT/LogsAutomation/LogsValidation.py
+++ ObservabilityAutomation-INT/LogsAutomation/LogsValidation.py
@@ -26,7 +26,8 @@ import LogForwardApps.Fluentbit_Logs
 import SendReport_to_GooleChat
 import SendReport_to_Slack
 
-config_and_report_directory = os.getcwd() + "/ObservabilityAutomation-INT"
+config_and_report_directory = os.path.dirname("ObservabilityAutomation-INT")
+print(config_and_report_directory)
 workdirectory = os.path.dirname(os.path.abspath(__file__))
 print(workdirectory)
 
